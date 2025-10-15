b43a325caf0b37c25f9d420f94b52915df152329
Karol <karol.porebski89@gmail.com>
2025-10-15 17:23:02 -0500

    chore: remove config.py from gitignore and add to tracking

---
+++ config.py
@@ -0,0 +1,9 @@
+# URLs for Navbar
+NAVBAR_URLS = {
+    'home': "/#top",
+    'project': "http://127.0.0.1:8050#projektbeschreibung",
+    'weather': "http://127.0.0.1:8050#wetterdaten",
+    'soil_moisture': "http://127.0.0.1:8050#bodenfeuchte",
+    'weitere_infos': "http://127.0.0.1:8050#hintergrund"
+
+}
